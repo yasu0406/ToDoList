88e70d9a8c7ccdf166d2cb79a4b7ce6eeecba5e1
katayama <yasucom4646@gmail.com>
2018-12-17 09:03:34 -0800

    Added TextInput

--- App.js
+++ App.js
@@ -7,43 +7,43 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+} from "react-native";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component<> {
+    state = {
+        newTodo: '',
+    }
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+    onChangeText(newTodo) {
+        this.setState({ newTodo })
+    }
+
+    render() {
+        console.log(this.state);
+        return (
+            <View styles={styles.container}>
+                <TextInput
+                    style={styles.form}
+                    onChangeText={text => this.onChangeText(text)}
+                />
+            </View>
+        )
+    }
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        padding: 30,
+    },
+    form: {
+        backgroundColor: '#EEE',
+        padding:10,
+        marginTop: 40,
+    }
 });
